6d7ff509d35d83b815d6681d38e2508af24b0c66
vic <mail@mail.com>
2023-12-14 12:19:46 -0300

    agregando calculo integral

--- electromagnetismo/libem.py
+++ electromagnetismo/libem.py
@@ -121,6 +121,125 @@ def plotEf(Ef, Q, dx, **params):
     plt.grid()
 
 
+#TODO: add 3d version.
+def plotEfcontribuciones(Ef, Q, x, **params):
+    """
+    Muestra los vectores de cada porción de un cuerpo extenso, ¡en 2D!
+    (no usar este código si las cargas están distribuidas en 3D).
+
+    Parameters
+    ----------
+    Ef : function
+        Una función de un campo vectorial (3 variables que devuelve 3 componentes).
+    Q : list
+        Q = [
+            [q1,x1,y1,z1],
+            [q2,x2,y2,z2],
+            ...
+            [qN,xN,yN,zN]
+        ]
+    X : tuple
+        Posición donde se calcula el campo.
+    limites : tuple
+        Lmites de los ejes: [xmin, xmax, ymin, ymax]
+    scale : float
+        Regula la longitud de las flechas.
+    r : float
+        Radio de las partículas cargadas.
+    linewidth : float
+        Grosor de las líneas que muestran la dirección.
+    in3D : bool
+        If True, it produces a 3D graph.
+
+    *Además de los parámetros de matplotlib y quiver, por ejemplo:*
+    length : float
+    figsize : tuple
+    title : string
+    """
+
+    figsize = params.get('figsize', (5,5))
+    title = params.get('title', "Contribuciones al campo eléctrico total")
+    scale = params.get('scale', 1)
+    linewidth = params.get('linewidth', 0.5)
+    in3D = params.get('in3D', False)
+
+    xmin, xmax, ymin, ymax, zmin, zmax = x[0],x[0],x[1],x[1], x[2], x[2]
+    x_pos = []
+    y_pos = []
+    z_pos = []
+    Ei = []
+    Ej = []
+    Ek = []
+    modulos = []
+    for q in Q:
+        Eii, Ejj, Ekk = Ef(x[0],x[1],x[2],[q])
+        # Elige límites para cuando el parámetro límites no es informado.
+        if q[1] > xmax:
+            xmax = q[1]
+        if q[1] < xmin:
+            xmin = q[1]
+        if q[2] > ymax:
+            ymax = q[2]
+        if q[2] < ymin:
+            ymin = q[2]
+        if q[3] > zmax:
+            zmax = q[3]
+        if q[3] < zmin:
+            zmin = q[3]
+        x_pos = np.concatenate((x_pos,x[0]), axis=None)
+        y_pos = np.concatenate((y_pos,x[1]), axis=None)
+        z_pos = np.concatenate((z_pos,x[2]), axis=None)
+        modulo = np.sqrt(Eii**2 + Ejj**2 + Ekk**2)
+        modulos = np.concatenate((modulos, modulo), axis=None)
+        Ei = np.concatenate((Ei, Eii), axis=None)
+        Ej = np.concatenate((Ej, Ejj), axis=None)
+        Ek = np.concatenate((Ek, Ekk), axis=None)
+
+    # Se expanden los límites automáticos:
+    xmax = xmax + (xmax - xmin)*0.2
+    xmin = xmin - (xmax - xmin)*0.2
+    ymax = ymax + (ymax - ymin)*0.2
+    ymin = ymin - (ymax - ymin)*0.2
+    zmax = zmax + (zmax - zmin)*0.2
+    zmin = zmin - (zmax - zmin)*0.2
+
+    rm = np.max(np.sqrt((xmax-xmin)**2 + (ymax-ymin)**2 + (zmax-zmin)**2))*0.01
+    r = params.get('r', rm)
+
+    Ei = np.round(Ei/np.max(modulos),3)
+    Ej = np.round(Ej/np.max(modulos),3)
+    Ek = np.round(Ek/np.max(modulos),3)
+
+    # Creating plot
+    if in3D:
+        pass
+    else:
+        fig, ax = plt.subplots(figsize = figsize)
+
+        # ax.quiver(x_pos, y_pos, Ei, Ej, angles='xy', scale_units='xy', scale=scale)
+        ax.quiver(x_pos, y_pos, Ei, Ej, scale=scale)
+
+        # Necesito un for separado para determinar el tamaño de los círculos.
+        for q in Q:
+            qq, xq, yq, zq = q
+
+            ax.plot([xq,x[0]], [yq,x[1]], color='b', linewidth=linewidth, linestyle='dashed')
+            if qq > 0:
+                colorq = 'red'
+            else :
+                colorq = 'green'
+            circ = plt.Circle((xq,yq), r, color=colorq)
+            ax.add_patch(circ)
+        # ax.set_title(title)
+        ax.set_xlabel('$x$ [m]')
+        ax.set_ylabel('$y$ [m]')
+        limites = params.get('limites', [xmin,xmax,ymin,ymax])
+
+    ax.axis(limites)
+    ax.set_title(title)
+    plt.show()
+    # plt.close()
+
 def plotEfvector(Ef, Q, X, **params):
     """
     Muestra las vectores del campo en 2D usando pyplot.quiver.
